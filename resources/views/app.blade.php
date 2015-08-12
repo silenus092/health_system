97e06f96c1c50a07f58f29408430fe145326d1ab
kunaphas_kongkitimanon <kunaphas.kon@gmail.com>
2015-08-12 17:33:12 +0700

    First Commit

---
+++ resources/views/app.blade.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Laravel</title>
+
+	<!-- CSS -->
+	<link rel="stylesheet" href="scripts/css/bootstrap.min.css">
+	<link rel="stylesheet" href="scripts/css/bootstrap-theme.min.css">
+	<link href="/css/app.css" rel="stylesheet">
+
+	<!-- Fonts -->
+	<link href="scripts/fonts/" rel='stylesheet' type='text/css'>
+	<!-- JS Scripts -->
+	<script src='scripts/jquery-1.11.2.min.js'></script>  
+	<script src="scripts/js/bootstrap.min.js"></script>
+	<style>
+
+	</style>
+
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+</head>
+<body>
+	<nav class="navbar navbar-inverse" role="navigation">
+		<div class="container-fluid">
+			<div class="navbar-header">
+
+				<img src="<?php  echo url('images/icbslogo1.png'); ?>" alt="ICBS" style="width:300px;height:50px;">
+			</div>
+
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				<ul class="nav navbar-nav">
+					<li><a href="/">Home</a></li>
+				</ul>
+
+				<ul class="nav navbar-nav navbar-right">
+					@if (Auth::guest())
+						<li><a href="/auth/login">Login</a></li>
+						<li><a href="/auth/register">Register</a></li>
+					@else
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
+							<ul class="dropdown-menu" role="menu">
+								<li><a href="/auth/logout">Logout</a></li>
+							</ul>
+						</li>
+					@endif
+				</ul>
+			</div>
+		</div>
+	</nav>
+
+	@yield('content')
+
+</body>
+</html>
